c566ae22b3e408a58b65640cebf8f6a8199b5d22
Rodrigo Lorandi <rodrigo.lorandi@southsystem.com.br>
2023-04-14 15:22:59 -0300

    adiciona projetos de cw2 no diretorio de ifrs

---
+++ 2-Semestre/Construcao_de_paginas_Web_II_Rodrigo_Prestes_Machado/1-cpw2-js/README.md
@@ -0,0 +1,23 @@
+# CPW2: JS
+
+Lista de exercícios de Javascript
+## Passos para a realização do exercício
+
+  * Primeiro, faça um [*fork*](https://docs.github.com/pt/get-started/quickstart/fork-a-repo) do projeto para a sua conta no Github
+  * Depois, realize um [clone](https://docs.github.com/pt/repositories/creating-and-managing-repositories/cloning-a-repository) para a sua máquina local
+  * Logo, implemente as [issues](https://github.com/rpmhub/cpw2-js/issues) que foram estipuladas para esse projeto
+  * Em seguida, realize um [push](https://docs.github.com/pt/desktop/contributing-and-collaborating-using-github-desktop/making-changes-in-a-branch/pushing-changes-to-github) da sua solução
+  * Finalmente, verifique na aba `Actions` do seu repositório no Github se os testes passaram (correção automática)
+
+🚨 Se você não souber como realizar operações como *fork*, *clone* e *push* no Github, por favor, assistam o [vídeo](https://www.youtube.com/watch?v=SDowGAvT0l0) com dicas de Git/Github.
+## Issues
+
+Existem três [issues](https://github.com/rpmhub/cpw2-js/issues) (tarefas) abertas no Github que devem ser implementadas. Estas [issues](https://github.com/rpmhub/cpw2-js/issues) detalham o que deve ser desenvolvidos no sistema a fim de permitir que os testes automáticos (correção) possam ser realizadas com sucesso. Assim, verifique as [issues](https://github.com/rpmhub/cpw2-js/issues)para você ter uma melhor compreensão do que deve ser desenvolvido nesse projeto.
+## Dicas 💡
+
+Se você desejar verificar se o seu projeto está correto, ou seja, os testes estão passando, execute o seguinte comando na raíz do seu projeto:
+
+```sh
+  npm install (apenas uma vez para instalar o mocha)
+  npm test
+```
---
+++ 2-Semestre/Construcao_de_paginas_Web_II_Rodrigo_Prestes_Machado/1-cpw2-js/index.js
@@ -0,0 +1,151 @@
+/**
+ * Recebe um array com os nomes de pessoas e retorna um array invertido
+ *
+ * @param {*} people : Um array contendo um conjunto de nomes
+ * @returns Um array com os nomes invertidos
+ */
+function invert(people) {
+  let nomesInvertidos = [];
+  for (let i = people.length - 1; i >= 0; i--) {
+    nomesInvertidos.push(people[i]);
+  }
+
+//   for (let i = 0; i < nomesInvertidos.length; i++) {
+//     console.log(nomesInvertidos[i]);
+//     console.log(people[i]);
+//   }
+  console.log(nomesInvertidos);
+  console.log(people);
+
+  return nomesInvertidos;
+}
+
+/**
+ * Essa função recebe um array de notas e retorna uma média
+ *
+ * @param {*} grades : Um array de notas
+ * @returns Uma média a partir do array de notas
+ */
+function mean(grades) {
+  let total = 0;
+  for (let i = 0; i < grades.length; i++) {
+    total += grades[i];
+  }
+  return total / grades.length;
+}
+
+/**
+ * Função recebe uma média e informa textualmente se um estudante está aprovado ou não
+ *
+ * @param {*} mean : Um número que representa uma média
+ * @returns Uma string dizendo se um estudante está aprovado ou não
+ */
+function isApproved(mean) {
+  if (mean >= 7) {
+    return "aprovado";
+  } else {
+    return "reprovado";
+  }
+}
+
+/**
+ * Recebe uma data no formato de string e transforma em um formato textual
+ *
+ * @param {*} strDate : Uma string no formato de data
+ * @returns Uma descrição da data informada
+ */
+function wide(strDate) {
+  if (strDate === "") {
+    return "";
+  }
+
+  let mesPorExtenso;
+  strDate = strDate.split("/");
+  let dia = strDate[0];
+  let mes = strDate[1];
+  let ano = strDate[2];
+
+  if (isNaN(dia) || isNaN(mes) || isNaN(ano)) {
+    return "";
+  } else {
+    let diaNumerico = parseInt(dia);
+    let mesNumerico = parseInt(mes);
+    let anoNumerico = parseInt(ano);
+    if (
+        diaNumerico < 1 ||
+        diaNumerico > 31 ||
+        mesNumerico < 1 ||
+        mesNumerico > 12 ||
+        anoNumerico < 0
+    ) {
+      return "";
+    }
+  }
+
+  switch (mes) {
+    case "01":
+      mesPorExtenso = "janeiro";
+      break;
+
+    case "02":
+      mesPorExtenso = "fevereiro";
+      break;
+
+    case "03":
+      mesPorExtenso = "março";
+      break;
+
+    case "04":
+      mesPorExtenso = "abril";
+      break;
+
+    case "05":
+      mesPorExtenso = "maio";
+      break;
+
+    case "06":
+      mesPorExtenso = "junho";
+      break;
+
+    case "07":
+      mesPorExtenso = "julho";
+      break;
+
+    case "08":
+      mesPorExtenso = "agosto";
+      break;
+
+    case "09":
+      mesPorExtenso = "setembro";
+      break;
+
+    case "10":
+      mesPorExtenso = "outubro";
+      break;
+
+    case "11":
+      mesPorExtenso = "novembro";
+      break;
+
+    case "12":
+      mesPorExtenso = "dezembro";
+      break;
+
+    default:
+      mesPorExtenso = mes;
+      break;
+  }
+
+  let data = dia + " de " + mesPorExtenso + " de " + ano;
+  return data;
+}
+
+invert(["Pedro", "Maria", "Trevis", "John", "Heitor", "Camila"]);
+
+// Trecho utilizado pelos testes
+exports._test = {
+  invert: invert,
+  mean: mean,
+  isApproved: isApproved,
+  wide: wide,
+};
