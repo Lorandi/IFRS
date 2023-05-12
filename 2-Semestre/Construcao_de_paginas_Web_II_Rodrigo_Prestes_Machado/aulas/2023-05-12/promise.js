5cd7855a37c4bca4b2082ae85fac55ef195ee492
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2023-05-12 14:25:32 +0000

    update

--- 2-Semestre/Construcao_de_paginas_Web_II_Rodrigo_Prestes_Machado/aulas/2023-05-12/promise.js
+++ 2-Semestre/Construcao_de_paginas_Web_II_Rodrigo_Prestes_Machado/aulas/2023-05-12/promise.js
@@ -1,4 +1,4 @@
-function asc(value) {
+async function asc(value) {
     return new Promise((resolve, reject) => {
         if(value){
             resolve("Promise resolvida com sucesso")
@@ -8,10 +8,22 @@ function asc(value) {
     });
 }
 
-function load(){
+//chamada da promise
+//  function load(){
 
-    var x = asc(false);
+//     var x = asc(false);
 
-    x.then((res) => {console.log(res)})
-    .catch((err) => {console.log(err)});
+//     x.then((res) => {console.log(res)})
+//     .catch((err) => {console.log(err)});
+// }
+
+//chamada da promise com async/await
+async function load(){
+    try{
+        var res = await asc(true);
+        console.log(res);
+    }catch(err){
+        console.log(err);
+    }
 }
+
