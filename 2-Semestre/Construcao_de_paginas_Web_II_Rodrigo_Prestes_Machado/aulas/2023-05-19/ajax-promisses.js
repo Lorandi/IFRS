cf9987809cbed28f1a490f0da451bff86ef14812
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2023-05-19 14:23:08 +0000

    update atividade promisses

--- 2-Semestre/Construcao_de_paginas_Web_II_Rodrigo_Prestes_Machado/aulas/2023-05-19/ajax-promisses.js
+++ 2-Semestre/Construcao_de_paginas_Web_II_Rodrigo_Prestes_Machado/aulas/2023-05-19/ajax-promisses.js
@@ -8,14 +8,11 @@ function myrequest(method, url) {
         xhr.onreadystatechange = () => {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 resolve(JSON.parse(xhr.responseText));
-            } else {
-                console.log(xhr.statusText)
-            }
+            } 
         }
 
         //tratamento de erro
         xhr.onerror = () => reject(xhr.statusText);
-
 
         //configurando a requisição
         xhr.open(method, url, true);
@@ -24,13 +21,35 @@ function myrequest(method, url) {
         xhr.send();
 
     });
+}
+ 
+async function main() {
+    try {
+        let images = await myrequest('GET', 'images.json');
+        console.log(images);
+        var divImages =document.getElementById('images');
+        images = shuffle(images);
 
-    async function main() {
-        try {
-            let objJson = await myrequest('GET', 'images.json');
-            console.log(objJson[1].alt);
-        } catch (error) {
-            console.log(error);
+        for(const image of images){
+            let img = document.createElement('img');
+            img.src = image.imagemUrl;
+            divImages.appendChild(img);
         }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * ( i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    } 
+    return array;  
+}
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        main();
     }
 }
