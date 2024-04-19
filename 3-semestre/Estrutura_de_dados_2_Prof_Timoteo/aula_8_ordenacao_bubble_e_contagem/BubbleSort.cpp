b05a2d3c7c662e1d8efd1a0d327eacc78cf5e4e4
Lorandi <rodrigo_lorandi@hotmail.com>
2024-04-18 22:21:41 -0300

    bubble sort

--- 3-semestre/Estrutura_de_dados_2_Prof_Timoteo/aula_8_ordenacao_bubble_e_contagem/BubbleSort.cpp
+++ 3-semestre/Estrutura_de_dados_2_Prof_Timoteo/aula_8_ordenacao_bubble_e_contagem/BubbleSort.cpp
@@ -4,23 +4,31 @@
 #include <stdlib.h>
 int main() {
 //Inicializaçã do Vetor
- int v[5]= {9,5,4,6,2}, n=5;
- int i, j = 0,  aux;
+ int v[5]= {8,1,13,19,28}, n=5;
+ int i, j = 0,  aux, flag = 0;
 //Algoritmo de Ordenação 
-	while (j < n)
-	{
-		for(i = 0; i < n-1; i++)
+	while (j < n){
+	
+		printf("\nj=%d", j);
+		flag = 0;
+		for(i = 0; i < n-1; i++){
+		
 			if(v[i] > v[i + 1])
 			{
              			aux=v[i];
              			v[i] = v[i+1];
              			v[i+1] = aux;
+             			flag = 1;
 			}
+		}			
+		if(flag==0)
+		 break;
 		j++;}
 //Laço de impressão do Vetor
+	printf("\nVector: ");
 	for (int q=0; q<5; q++)
 	{
-        printf("%d \n",v[q]);
+        printf("%d ",v[q]);
     }
     system("pause");  
 }
